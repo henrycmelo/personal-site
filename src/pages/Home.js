83465d3e0002f0c87a8c62f324aa4591fa6a8163
Henry Castillo <henrycastillome@gmail.com>
2023-03-03 10:13:45 -0500

    changes

---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+
+import Header from "../components/Header"
+import LandingSection from "../components/LandingSection";
+import AboutMeSection from "../components/AboutMeSection";
+import ProjectsSection from "../components/ProjectsSection";
+import ContactMeSection from "../components/ContactMeSection";
+import Footer from "../components/Footer";
+
+import Alert from "../components/Alert";
+
+
+
+
+
+function Home() {
+  return (
+    <main>
+          <Header />
+          <LandingSection />
+          <AboutMeSection /> 
+          <ProjectsSection />
+          <ContactMeSection />
+          <Footer />
+          <Alert />
+    </main>
+        
+     
+  );
+}
+
+export default Home;
