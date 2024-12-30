7ecb0cfd1ee6cf06c9a479d01cbbe8f547e249de
Henry Castillo <henrycastillome@gmail.com>
2024-12-30 17:13:54 -0500

    working on it

---
+++ src/components/VerticalProgressBar.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Box, VStack } from "@chakra-ui/react";
+
+const VerticalProgressBar = ({ sections }) => {
+  const [activeSection, setActiveSection] = useState("");
+
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    section?.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let currentSection = "";
+      sections?.forEach((section) => {
+        const element = document.getElementById(section.id);
+        if (element) {
+          const rect = element.getBoundingClientRect();
+          if (
+            rect.top <= window.innerHeight / 2 &&
+            rect.bottom >= window.innerHeight / 2
+          ) {
+            currentSection = section.id;
+          }
+        }
+      });
+      setActiveSection(currentSection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [sections]);
+
+  return (
+    <VStack position="sticky" top="20%" left="5%" zIndex="10">
+      {sections?.map((section) => (
+        <Box
+          textStyle="caption"
+          w={"100%"}
+          key={section.id}
+          onClick={() => scrollToSection(section.id)}
+          color={
+            activeSection === section.id
+              ? "semantic.background.primary"
+              : "semantic.text.primary"
+          }
+          fontWeight={activeSection === section.id ? "bold" : "normal"}
+          backgroundColor={
+            activeSection === section.id ? "semantic.accent" : "none"
+          }
+          cursor="pointer"
+          transition={'background-color 0.3s'}
+          p={2}
+        >
+          <Box>{section.label}</Box>
+        </Box>
+      ))}
+    </VStack>
+  );
+};
+
+export default VerticalProgressBar;
--- src/pages/CianaResearch.js
+++ src/pages/CianaResearch.js
@@ -1,14 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import FullScreenSection from "../components/FullScreenSection";
-import { Box, VStack, Text, Image, Grid, GridItem, Divider } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Text,
+  Image,
+  Grid,
+  GridItem,
+  Divider,
+  HStack,
+} from "@chakra-ui/react";
 import { useAlertContext } from "../context/alertContext";
+import VerticalProgressBar from "../components/VerticalProgressBar";
 
 const CianaResearch = () => {
   const { capitalizeEachWord } = useAlertContext();
+  const sections = [
+    { id: "theproject", label: "The Project" },
+    { id: "analysis", label: "Analysis" },
+    { id: "design", label: "Design" },
+    { id: "deliver", label: "Deliver" },
+  ];
+
+  const findId = (label) => {
+    if (!sections) {
+      throw new Error("Section array is not defined");
+    }
+
+    const sectionObject = sections.find(
+      (section) => section.label?.toLowerCase() === label.toLowerCase()
+    );
+
+    if (!sectionObject) {
+      console.error(`No section found for label: ${label}`);
+      return null;
+    }
+
+    return sectionObject.id;
+  };
   return (
     <>
       <Header />
+
       <FullScreenSection>
         <VStack color="semantic.text.primary" alignItems="start">
           <Text as="h1" textStyle={"h1"}>
@@ -34,34 +68,153 @@ const CianaResearch = () => {
               w="100%"
             />
           </Box>
-          <Grid w={"100%"}  templateColumns="repeat(3, 1fr)" gap={24} >
-            <GridItem w='100%'>
-                <VStack alignItems='start'>
-                    <Text as='p' textStyle='pbold' >
-                        About the product
-                    </Text>
-                    <Text as='p' textStyle={'p'}>CIANA is not for profit organization that offer different social services to inmigrants</Text>
-                </VStack>
+          <Grid w={"100%"} templateColumns={{base:"1fr", md:"repeat(4, 1fr)"}} gap={{base:2, md:24}}>
+            <GridItem w="100%">
+              <VStack alignItems="start">
+                <Text as="p" textStyle="pbold">
+                  Role
+                </Text>
+                <Text as="p" textStyle={"p"} >
+                Lorem ipsum dolor sit amet
+                </Text>
+              </VStack>
             </GridItem>
-            <GridItem w='100%'><VStack alignItems='start'>
-                    <Text as='p' textStyle='pbold' >
-                        Role
-                    </Text>
-                    <Text as='p' textStyle={'p'}>UX Researcher</Text>
-                </VStack>
-                </GridItem>
-            <GridItem w='100%'><VStack alignItems='start'>
-                    <Text as='p' textStyle='pbold' >
-                        Main Project Goal
-                    </Text>
-                    <Text as='p' textStyle={'p'}>Identify key themes and provide actionable recommendations to improve future workshops in the grant report for the citywide Participatory Budgeting</Text>
-                </VStack>
-                </GridItem>
-          </Grid>
+            <GridItem w="100%">
+              <VStack alignItems="start">
+                <Text as="p" textStyle="pbold">
+                  Duration
+                </Text>
+                <Text as="p" textStyle={"p"}>
+                Lorem ipsum dolor sit amet
+                </Text>
+              </VStack>
+            </GridItem>
+            <GridItem w="100%">
+              <VStack alignItems="start">
+                <Text as="p" textStyle="pbold">
+                  Tools
+                </Text>
+                <Text as="p" textStyle={"p"}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. 
+                </Text>
+              </VStack>
+            </GridItem>
 
+            <GridItem w="100%">
+              <VStack alignItems="start">
+                <Text as="p" textStyle="pbold">
+                  Skills
+                </Text>
+                <Text as="p" textStyle={"p"}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. nec massa
+                </Text>
+              </VStack>
+            </GridItem>
+          </Grid>
         </VStack>
 
-        <Divider variant='thick'  />
+        <Divider variant="thick" />
+        <Grid
+          templateColumns={{base:"1fr", md:"200px 1fr"}}
+          gap={6}
+          height="100vh"
+          alignItems="start"
+          w={"100%"}
+        >
+          <Box display={{base:'none', md:'block'}} visibility={{base:'hidden', md:'visible'}}>
+            <VerticalProgressBar sections={sections} />
+          </Box>
+
+          <Box overflowY="scroll">
+
+            {/* ALL CONTENT HERE */}
+            <Box id={findId("the project")}>
+              <VStack alignItems="start">
+                <Text textStyle="h2">Overview</Text>
+                
+                <Grid templateColumns={{base:"1fr", md:"1fr 1fr"}}>
+
+                  {/* Text Columns */}
+                  <GridItem>
+                    <Box pb={4}>
+                      <Text textStyle='pbold' >
+                        {"Client".toUpperCase()}
+                      </Text>
+                      <Text textStyle='p'>
+                      Corona community, Queen, NY (The People’s Money grant by NYC Civic Engagement Commision)
+                      </Text>
+                    </Box>
+
+                    <Box pb={4}>
+                      <Text textStyle='pbold' >
+                        {"Challenge".toUpperCase()}
+                      </Text>
+                      <Text textStyle='p' >
+                      Evaluate and improve digital safety workshops' effectiveness for parents
+                      </Text>
+                    </Box>
+
+                    <Box pb={4}>
+                      <Text textStyle='pbold' >
+                        {"My impact".toUpperCase()}
+                      </Text>
+                      <Text textStyle='p' >
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque.
+                      </Text>
+                    </Box>
+                    
+                  </GridItem>
+
+                  {/* image Column */}
+                  <GridItem>
+                    <Box>
+                        <Image
+                            src="https://bit.ly/naruto-sage"
+                            alt="Placeholder"
+                            objectFit="cover"
+                            width="100%"
+                            height="100%" // Makes the image fill the available space
+                        />
+                    </Box>
+                </GridItem>
+                </Grid>
+
+                {/* Second Grip */}
+
+                <Grid templateColumns={{base:"1fr", md:"1fr 1fr"}}>
+
+                  {/* Text Column 1 */}
+                  <GridItem>
+                    <Box pb={4}>
+                      <Text textStyle='pbold' >
+                        {"Goal".toUpperCase()}
+                      </Text>
+                      <Text textStyle='p'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
+                      </Text>
+                    </Box>
+
+                    
+                  </GridItem>
+
+                  {/* Text Column 2 */}
+                  <GridItem>
+                  <Box pb={4}>
+                      <Text textStyle='pbold' >
+                        {"Problem".toUpperCase()}
+                      </Text>
+                      <Text textStyle='p'>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
+                      </Text>
+                    </Box>
+                </GridItem>
+                </Grid>
+
+
+              </VStack>
+            </Box>
+          </Box>
+        </Grid>
       </FullScreenSection>
     </>
   );
