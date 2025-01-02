c536120a203d665533a7e8e768ef3ef9b4f63473
Henry Castillo <henrycastillome@gmail.com>
2025-01-02 18:41:49 -0500

    fixing

--- src/pages/CianaResearch.js
+++ src/pages/CianaResearch.js
@@ -13,6 +13,8 @@ import {
 } from "@chakra-ui/react";
 import { useAlertContext } from "../context/alertContext";
 import VerticalProgressBar from "../components/VerticalProgressBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLightbulb, faListCheck, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 const CianaResearch = () => {
   const { capitalizeEachWord } = useAlertContext();
@@ -55,7 +57,9 @@ const CianaResearch = () => {
           </Text>
           <Text as="hp" textStyle={"p"}>
             {" "}
-            {capitalizeEachWord("Empowering Parents in Corona, Queens with Digital Safety Knowledge")}
+            {capitalizeEachWord(
+              "Empowering Parents in Corona, Queens with Digital Safety Knowledge"
+            )}
           </Text>
           <Text as="p" textStyle={"p"}>
             2024
@@ -72,15 +76,19 @@ const CianaResearch = () => {
               w="100%"
             />
           </Box>
-          <Grid w={"100%"} templateColumns={{base:"1fr", md:"repeat(5, 1fr)"}} gap={{base:2, md:24}}>
-          <GridItem w={{base: '50%', md:"100%"}}>
+          <Grid
+            w={"100%"}
+            templateColumns={{ base: "1fr", md: "repeat(5, 1fr)" }}
+            gap={{ base: 2, md: 24 }}
+          >
+            <GridItem w={{ base: "50%", md: "100%" }}>
               <VStack alignItems="start">
                 <Text as="p" textStyle="pbold">
                   Client
                 </Text>
-                <Divider variant='thick'/>
-                <Text as="p" textStyle={"p"} >
-                Lorem ipsum dolor sit amet
+                <Divider variant="thick" />
+                <Text as="p" textStyle={"p"}>
+                  Lorem ipsum dolor sit amet
                 </Text>
               </VStack>
             </GridItem>
@@ -89,9 +97,9 @@ const CianaResearch = () => {
                 <Text as="p" textStyle="pbold">
                   Role
                 </Text>
-                <Divider variant='thick'/>
-                <Text as="p" textStyle={"p"} >
-                Lorem ipsum dolor sit amet
+                <Divider variant="thick" />
+                <Text as="p" textStyle={"p"}>
+                  Lorem ipsum dolor sit amet
                 </Text>
               </VStack>
             </GridItem>
@@ -100,9 +108,9 @@ const CianaResearch = () => {
                 <Text as="p" textStyle="pbold">
                   Duration
                 </Text>
-                <Divider variant='thick'/>
+                <Divider variant="thick" />
                 <Text as="p" textStyle={"p"}>
-                Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
                 </Text>
               </VStack>
             </GridItem>
@@ -111,9 +119,9 @@ const CianaResearch = () => {
                 <Text as="p" textStyle="pbold">
                   Tools
                 </Text>
-                <Divider variant='thick'/>
+                <Divider variant="thick" />
                 <Text as="p" textStyle={"p"}>
-                Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
                 </Text>
               </VStack>
             </GridItem>
@@ -123,9 +131,9 @@ const CianaResearch = () => {
                 <Text as="p" textStyle="pbold">
                   Skills
                 </Text>
-                <Divider variant='thick'/>
+                <Divider variant="thick" />
                 <Text as="p" textStyle={"p"}>
-                Lorem ipsum dolor sit amet
+                  Lorem ipsum dolor sit amet
                 </Text>
               </VStack>
             </GridItem>
@@ -133,154 +141,172 @@ const CianaResearch = () => {
         </VStack>
 
         <Divider variant="section" />
-        
+
         <Grid
-          templateColumns={{base:"1fr", md:"200px 10px 1fr"}}
+          templateColumns={{ base: "1fr", md: "200px 10px 1fr" }}
           gap={6}
           height="100vh"
           alignItems="start"
           w={"100%"}
           pt={4}
         >
-          <Box display={{base:'none', md:'block'}} visibility={{base:'hidden', md:'visible'}}>
+          <Box
+            display={{ base: "none", md: "block" }}
+            visibility={{ base: "hidden", md: "visible" }}
+          >
             <VerticalProgressBar sections={sections} />
-            
           </Box>
-          <Divider orientation="vertical" variant='thick' />
-          
-          
+          <Divider orientation="vertical" variant="thick" />
 
           <Box overflowY="scroll">
-
             {/* ALL CONTENT HERE */}
             <Box id={findId("the project")}>
               <VStack alignItems="start">
                 <Text textStyle="h2">Overview</Text>
-                
-                <Grid templateColumns={{base:"1fr", md:"1fr 1fr"}}>
 
+                <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }}>
                   {/* Text Columns */}
                   <GridItem>
-                  <Box pb={4}>
-                      <Text textStyle='pbold' >
+                    <Box pb={4}>
+                      <Text textStyle="pbold">
                         {"Background".toUpperCase()}
                       </Text>
-                      <Text textStyle='p' >
-                      Evaluate and improve digital safety workshops' effectiveness for parents
+                      <Text textStyle="p">
+                        Evaluate and improve digital safety workshops'
+                        effectiveness for parents
                       </Text>
                     </Box>
 
                     <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Relevance".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p' >
-                      Evaluate and improve digital safety workshops' effectiveness for parents
+                      <Text textStyle="pbold">{"Relevance".toUpperCase()}</Text>
+                      <Text textStyle="p">
+                        Evaluate and improve digital safety workshops'
+                        effectiveness for parents
                       </Text>
                     </Box>
-                    
 
                     <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"My impact".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p' >
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque.
+                      <Text textStyle="pbold">{"My impact".toUpperCase()}</Text>
+                      <Text textStyle="p">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nam hendrerit nisi sed sollicitudin pellentesque.
                       </Text>
                     </Box>
-                    
                   </GridItem>
 
                   {/* image Column */}
                   <GridItem>
                     <Box>
-                        <Image
-                            src="https://bit.ly/naruto-sage"
-                            alt="Placeholder"
-                            objectFit="cover"
-                            width="100%"
-                            height="100%" // Makes the image fill the available space
-                        />
+                      <Image
+                        src="https://bit.ly/naruto-sage"
+                        alt="Placeholder"
+                        objectFit="cover"
+                        width="100%"
+                        height="100%" // Makes the image fill the available space
+                      />
                     </Box>
-                </GridItem>
+                  </GridItem>
                 </Grid>
 
                 {/* Quick stats */}
 
-                <Box backgroundColor={'red'} >
-                    <Grid templateColumns={{base:"1fr", md:"repeat(3, 1fr)"}}>
-                    <GridItem>
-                    <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Goal".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
-                      </Text>
-                    </Box>
+                <Box
+                  backgroundColor={"semantic.background.secondary"}
+                  w={"100%"}
+                >
+                  <VStack alignItems="center">
+                    <Text textStyle="h2">Quick Stats</Text>
 
-                    
-                  </GridItem>
+                    <Grid
+                      templateColumns={{ base: "1fr", md: "repeat(3, 1fr)" }}
+                      gap={6}
+                      textAlign={"center"}
+                    >
+                      <GridItem>
+                        <Box pb={4}>
+                          <HStack color={'semantic.accent'} align={'center'} justifyContent={'center'}>
+                            <FontAwesomeIcon
+                              icon={faUserGroup}
+                              size="xl"
+                            />
+                            <Text as="p" textStyle="h3" >
+                              7
+                            </Text>
+                          </HStack>
 
-                  {/* Text Column 2 */}
-                  <GridItem>
-                  <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Problem".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
-                      </Text>
-                    </Box>
-                </GridItem>
+                          <Text textStyle="p">{"Total Interviews"}</Text>
+                        </Box>
+                      </GridItem>
 
-                {/* Text Column 4 */}
-                <GridItem>
-                  <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Problem".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
-                      </Text>
-                    </Box>
-                </GridItem>
+                      {/* Text Column 2 */}
+                      <GridItem>
+                        <Box pb={4}>
+                        <HStack color={'semantic.accent'} align={'center'} justifyContent={'center'}>
+                            <FontAwesomeIcon
+                              icon={faListCheck}
+                              size="xl"
+                              
+                            />
+                            <Text as="p" textStyle="h3" >
+                              8
+                            </Text>
+                          </HStack>
+                          <Text textStyle="p">{"Identified themes"}</Text>
+                        </Box>
+                      </GridItem>
+
+                      {/* Text Column 4 */}
+                      <GridItem>
+                        <Box pb={4}>
+                        <HStack color={'semantic.accent'} align={'center'} justifyContent={'center'}>
+                            <FontAwesomeIcon
+                              icon={faLightbulb}
+                              size="xl"
+                            />
+                            <Text as="p" textStyle="h3" >
+                              4
+                            </Text>
+                          </HStack>
+                          <Text textStyle="p">{"Recommendations"}</Text>
+                        </Box>
+                      </GridItem>
                     </Grid>
-
+                  </VStack>
                 </Box>
 
                 {/* Second Grid */}
 
-                <Grid templateColumns={{base:"1fr", md:"1fr 1fr"}}>
-
+                <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }}>
                   {/* Text Column 1 */}
                   <GridItem>
                     <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Goal".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
+                      <Text textStyle="pbold">{"Goal".toUpperCase()}</Text>
+                      <Text textStyle="p">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nam hendrerit nisi sed sollicitudin pellentesque. Nunc
+                        posuere purus rhoncus pulvinar aliquam. Ut aliquet
+                        tristique nisl vitae volutpat. Nulla aliquet porttitor
+                        venenatis. Donec a dui et dui fringilla consectetur id
+                        nec massa
                       </Text>
                     </Box>
-
-                    
                   </GridItem>
 
                   {/* Text Column 2 */}
                   <GridItem>
-                  <Box pb={4}>
-                      <Text textStyle='pbold' >
-                        {"Problem".toUpperCase()}
-                      </Text>
-                      <Text textStyle='p'>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam hendrerit nisi sed sollicitudin pellentesque. Nunc posuere purus rhoncus pulvinar aliquam. Ut aliquet tristique nisl vitae volutpat. Nulla aliquet porttitor venenatis. Donec a dui et dui fringilla consectetur id nec massa
+                    <Box pb={4}>
+                      <Text textStyle="pbold">{"Problem".toUpperCase()}</Text>
+                      <Text textStyle="p">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nam hendrerit nisi sed sollicitudin pellentesque. Nunc
+                        posuere purus rhoncus pulvinar aliquam. Ut aliquet
+                        tristique nisl vitae volutpat. Nulla aliquet porttitor
+                        venenatis. Donec a dui et dui fringilla consectetur id
+                        nec massa
                       </Text>
                     </Box>
-                </GridItem>
+                  </GridItem>
                 </Grid>
-
-
               </VStack>
             </Box>
             <Divider variant="thick" />
